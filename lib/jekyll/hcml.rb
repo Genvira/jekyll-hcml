68f0c1a93ce0d26b8accb8cc21060a5b007eacc2
Aaron Li <aaron.li@genvira.com>
2025-02-22 12:53:02 -0500

    🎉 Create the gem

---
+++ lib/jekyll/hcml.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hcml/version"
+
+module Jekyll
+  module Hcml
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/jekyll/hcml_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jekyll::Hcml do
+  it "has a version number" do
+    expect(Jekyll::Hcml::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
