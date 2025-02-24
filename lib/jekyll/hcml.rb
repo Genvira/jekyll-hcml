6b2aa2e3137fb07f5f731b754336d9726f4ea66d
Aaron Li <aaron.li@genvira.com>
2025-02-24 11:03:26 -0500

    ✨ Run HCML command to convert to HTML

--- lib/jekyll/hcml.rb
+++ lib/jekyll/hcml.rb
@@ -2,9 +2,47 @@
 
 require_relative "hcml/version"
 
+require "open3"
+
 module Jekyll
-  module Hcml
-    class Error < StandardError; end
-    # Your code goes here...
+  # module Hcml
+  #   class Error < StandardError; end
+  #   # Your code goes here...
+  # end
+
+  class HcmlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /^\.hcml$/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      # puts "Content: #{content}"
+      # result = `hcml -`
+      # puts "Result: #{result}"
+      # result
+
+      Jekyll.logger.info "Converting HCML to HTML:"
+
+      stdout, stderr, status = Open3.capture3("hcml -", stdin_data: content)
+
+      # Print stderr if it is not empty
+      unless stderr.strip.empty?
+        Jekyll.logger.warn stderr
+      end
+
+      # If the command failed, raise an error.
+      unless status.success?
+        raise status.to_s
+      end
+
+      stdout
+    end
   end
 end
